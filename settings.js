7fdb0d8b90fb41f0d1a898c6f940e2d7e7487adb
Jephter-Tech <alexkenneth935@gmail.com>
2025-08-25 13:50:41 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'JEPHTER TECH',
   botName: "☞ ᴊꜰx ᴍᴅ-xᴠ2",
   botOwner: '♛ᴊᴀʜ-ᴍᴀꜱᴛᴇʀ', // Your name
-  ownerNumber: '447721869191, // No + symbol
+  ownerNumber: '+447721869191, // No + symbol
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴡʜᴀᴛꜱᴀᴘᴘ ᴍᴅ ʙᴏᴛ ʙʏ ᴊᴇᴘʜᴛᴇʀ ᴛᴇᴄʜ. ᴄʜᴀɴɴᴇʟ: https://whatsapp.com/channel/0029VbAxkJl0lwgqAOojKI3R",
